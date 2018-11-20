9a245d358a8f40158124c1e12ead4a0715b70ca4
gabe3 <Dinya7991>
2018-11-19 21:02:39 -0500

    Task: Made the axis but the ticks are overlapping each other.

--- docs/heatmap.js
+++ docs/heatmap.js
@@ -1,4 +1,4 @@
-const width = 800;
+const width = 2600;
 const height= 500;
 
 
@@ -12,13 +12,29 @@ d3.csv("cereals.csv", function(cereals){
     let data = cereals.map((d)=>{
         return{
             x: d[name],
-            y: parseInt(d[rating])
+            y: parseFloat(d[rating])
         };
-    })
+    });
+    //Puts both attributes in separate arrays.
+    let cerealName = cereals.map((d)=>{return d[name]});
+    let cerealRating = cereals.map((d)=> {return d[rating]});
 
     //This removes the first object in the data array so that all we have is data.
     data.shift();
+    cerealName.shift();
+    cerealRating.shift();
 
-    
-    console.log(data[0]);
+
+
+    //Make the Axis for the Names
+    let svg = d3.select("svg").attr("height", height).attr("width",width);
+
+
+    let xScale = d3.scaleBand().domain(cerealName).range([0,width]);
+    let xAxis = d3.axisTop().scale(xScale).tickSizeInner(20);
+    svg.append("g")
+        .call(xAxis)
+        .attr("transform","translate(10,200)");
+
+
 });
