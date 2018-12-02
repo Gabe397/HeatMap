c47b0fcf3216c92a10befc47c31bc85132c5a2bd
gabe3 <Dinya7991>
2018-12-01 21:48:21 -0500

    Feature: Created a way where if you hover it shows the quality value.

--- docs/heatmap.js
+++ docs/heatmap.js
@@ -46,8 +46,11 @@ d3.csv("cereals.csv", function(cereals){
 
 
 
+    //Make Tooltips
+
+
     //Make Color Scale
-    let colorScale = d3.scaleLinear().domain([d3.min(cerealRating), d3.max(cerealRating)]).range(["rgb(255,0,0)","rgb(0,0,255)" ]);
+    let colorScale = d3.scaleLinear().domain([0, d3.max(cerealRating)]).range(["rgb(255,0,0)","rgb(0,0,255)" ]);
 
     //Make the Cells
     let cell = svg.selectAll("rect").data(data);
@@ -59,7 +62,9 @@ d3.csv("cereals.csv", function(cereals){
                 .attr("height",cellSize)
                 .attr("y",function(d){return yScale(d.y)})
                 .attr("transform","translate(151.8,50)")
-                .attr("fill", function(d){return colorScale(d.x)});
+                .attr("fill", function(d){return colorScale(d.x)})
+                .append("svg:title")
+                .text(function(d) { return d.x; });;
 
 
 
